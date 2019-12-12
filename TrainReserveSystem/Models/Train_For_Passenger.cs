59d44b4c30d1aac719a2dcab877d8b7bb6d86ce7
poddarashit <55320394+poddarashit@users.noreply.github.com>
2019-12-12 19:48:11 -0400

    search page view and  custom model
    search page view and  custom model

---
+++ TrainReserveSystem/Models/Train_For_Passenger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Web;
+
+namespace TrainReserveSystem.Models
+{
+    public class Train_For_Passenger
+    {
+        //DbSet<Train_For_Passenger> trains { get; set; }
+        public int Train_Number { get; set; }
+        public string Train_Name { get; set; }
+        public double Fare { get; set; }
+
+    }
+}
